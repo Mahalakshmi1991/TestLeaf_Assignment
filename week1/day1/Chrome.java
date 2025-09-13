1cf2c393a122beff2c054f4c20da7aecaa6574c9
Mahalakshmi1991 <mahalakshmikaveri@gmail.com>
2025-09-13 23:58:39 +0530

    Add files via upload
    week1 ->Day1 Assignment

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Version is " + version);
+		System.out.println("Developer is " + developer);
+		System.out.println("isBeta is " + isBeta);
+		System.out.println("releaseYear is " + releaseYear);
+		System.out.println("shortcutKey is " + shortcutKey);
+
+	}
+
+}
